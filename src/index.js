3b56e7aa13087335016d4886a3ffaa95acc284a0
sergei-dev <antonuk-sergei@rambler.ru>
2018-01-10 11:06:29 +0100

    fix reacr dz 4

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import registerServicesWorker from './registerServiceWorker';
 
 const root = document.querySelector('#root');
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename="Movie_Mate/build/">
         <App />
     </BrowserRouter>,    
     root
